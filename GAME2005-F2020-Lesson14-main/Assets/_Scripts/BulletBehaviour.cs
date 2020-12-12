5242a1d3f5ae68d1142fcd68177218c899f138d1
david <eorjs2020@gmail.com>
2020-12-12 13:19:12 -0500

    Initial

---
+++ GAME2005-F2020-Lesson14-main/Assets/_Scripts/BulletBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletBehaviour : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public float range;
+
+    private MeshFilter meshFilter;
+    public bool isColliding;
+  
+    // Start is called before the first frame update
+    void Start()
+    {        
+        isColliding = false;
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void _CheckBounds()
+    {
+        if (Vector3.Distance(transform.position, Vector3.zero) > range)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GAME2005-F2020-Lesson14-main/Assets/_Scripts/CollisionManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CollisionManager : MonoBehaviour
+{
+    public CubeBehaviour[] actors;
+    public BulletBehaviour[] bullet;
+    public Vector3 collisionPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        actors = FindObjectsOfType<CubeBehaviour>();
+        
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        bullet = FindObjectsOfType<BulletBehaviour>();
+        /*for (int i = 0; i < bullet.Length; i++)
+        {
+            bullet[i].isColliding = false;
+        }
+        for (int i = 0; i < actors.Length; i++)
+        {
+            actors[i].isColliding = false;
+        }*/
+
+
+        for (int i = 0; i < bullet.Length; i++)
+        {
+            for (int j = 0; j < actors.Length; j++)
+            {
+                if (i != j)
+                {
+                    CheckAABBandSphere(bullet[i], actors[j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < bullet.Length; i++)
+        {
+            for (int j = 0; j < actors.Length; j++)
+            {
+                if (i != j)
+                {
+                    CheckAABBs(actors[i], actors[j]);
+                }
+            }
+        }
+    }
+
+    public static void CheckAABBs(CubeBehaviour a, CubeBehaviour b)
+    {
+        if ((a.min.x <= b.max.x && a.max.x >= b.min.x) &&
+            (a.min.y <= b.max.y && a.max.y >= b.min.y) &&
+            (a.min.z <= b.max.z && a.max.z >= b.min.z))
+        {
+            if (!a.contacts.Contains(b))
+            {
+                a.contacts.Add(b);
+                a.isColliding = true;
+            }
+        }
+        else
+        {
+            if (a.contacts.Contains(b))
+            {
+                a.contacts.Remove(b);
+                a.isColliding = false;
+            }
+
+        }
+    }
+    public static void CheckAABBandSphere(BulletBehaviour a, CubeBehaviour b)
+    {
+        float radius = 0.05f;
+        var x = Mathf.Max(b.min.x, Mathf.Min(a.transform.position.x, b.max.x));
+        var y = Mathf.Max(b.min.y, Mathf.Min(a.transform.position.y, b.max.y));
+        var z = Mathf.Max(b.min.z, Mathf.Min(a.transform.position.z, b.max.z));
+
+        var distance = Mathf.Sqrt((x - a.transform.position.x) * (x - a.transform.position.x) +
+            (x - a.transform.position.y) * (x - a.transform.position.y) +
+            (x - a.transform.position.z) * (x - a.transform.position.z));
+
+        if(distance < radius)
+        {
+            a.isColliding = true;
+            b.isColliding = true;
+        }
+
+    }
+}
