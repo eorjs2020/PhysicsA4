d9ea7ee723a2a52e06176a3ce865bc7853f56484
Mick <mickx.shular@gmail.com>
2020-12-12 19:25:13 -0500

    Wall bounce

--- GAME2005-F2020-Lesson14-main/Assets/_Scripts/CollisionManager.cs
+++ GAME2005-F2020-Lesson14-main/Assets/_Scripts/CollisionManager.cs
@@ -92,5 +92,84 @@ public class CollisionManager : MonoBehaviour
             b.isColliding = true;
         }
 
+
+        Vector3 temp;
+        temp.x = Mathf.Abs(a.transform.position.x - b.transform.position.x);
+        temp.y = Mathf.Abs(a.transform.position.y - b.transform.position.y);
+        temp.z = Mathf.Abs(a.transform.position.z - b.transform.position.z);
+
+        temp.x = temp.x / Mathf.Sqrt(temp.x);
+        temp.y = temp.y / Mathf.Sqrt(temp.y);
+        temp.z = temp.z / Mathf.Sqrt(temp.z);
+
+  
+        float smalltemp = 0.0f;
+        float bigtemp = 0.0f;
+        float difftemp = 0.0f;
+        bool xs = false, xb = false, ys = false, yb = false, zs = false, zb = false;
+        System.Console.WriteLine(temp.x);
+
+
+        if (temp.x < temp.y)
+        {
+            smalltemp = temp.y;
+            bigtemp = temp.x;
+            xb = true;
+            ys = true;
+        }
+        else
+        {
+            bigtemp = temp.y;
+            smalltemp = temp.x;
+            yb = true;
+            xs = true;
+        }
+        if (z < smalltemp)
+        {
+            smalltemp = temp.z;
+            xs = false;
+            ys = false;
+            zs = true;
+        }
+        if (z > bigtemp)
+        {
+            bigtemp = temp.z;
+            xb = false;
+            yb = false;
+            zb = true;
+        }
+        difftemp = 1 - bigtemp;
+
+        if (difftemp < smalltemp)
+        {
+            xs = false;
+            ys = false;
+            zs = false;
+        }
+        else
+        {
+            xb = false;
+            yb = false;
+            zb = false;
+        }
+        
+        if (zb || zs)
+        {
+            a.direction.z = a.direction.z * -1;
+        }
+        if (xb || xs)
+        {
+            a.direction.x = a.direction.x * -1;
+        }
+        if (yb || ys)
+        {
+            a.direction.y = a.direction.y * -1;
+        }
+
+        a.isColliding = true;
+        b.isColliding = true;
+
+
+
     }
 }
